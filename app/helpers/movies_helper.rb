05c6b6851be9de2d61f8db912dc28edfe98267d0
root <root@LAPTOP-602MB4IK>
2024-09-10 11:43:31 -0500

    change style of input fields in create/update views

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,4 +2,19 @@ module MoviesHelper
     def toggle_direction(current_direction)
         current_direction == 'asc' ? 'desc' : 'asc'
     end
+    
+    def sort_indicator(column)
+        if params[:sort] == column
+          params[:direction] == 'asc' ? '↑' : '↓'
+        end
+    end
+    
+    # Checks if the column is currently being sorted
+    def sorted_class(column)
+        if params[:sort] == column
+          params[:direction] == 'asc' ? 'sorted-asc' : 'sorted-desc'
+        end
+      end
+    end
+
 end
